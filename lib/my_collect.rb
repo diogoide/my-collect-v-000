0e4b909887ad587935ab6d01d5cb2e8db403ae99
Diogo Ide <diogoide@gmail.com>
2018-04-04 22:58:21 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,11 +1,14 @@
 def  my_collect(collection)
   i = 0
   while i < collection.size
+    if
     yield collection[i]
     i += 1
   end
 end
     #return modified collection
 
-my_collect(collection) do |name|
+my_collect(collection) do |name, language|
   name.split(" ").first
+  language.upcase
+end 
