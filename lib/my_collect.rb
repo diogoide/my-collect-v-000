c14529a068632d989aa58434f26629628d8b7e26
Diogo Ide <diogoide@gmail.com>
2018-04-04 23:20:23 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -13,11 +13,13 @@ end
     #return modified collection
 students = ["Tim Jones", "Tom Smith", "Sophie Johnson", "Antoin Miller"]
 my_collect(students) do |name|
-   name.split(" ").first
+   name.split(" ").first = first_name
+   return first_name
 end
 
 
 languages = ["ruby,", "javascript", "python", "objective-c"]
 my_collect(languages) do |lang|
-   lang.upcase
+   lang.upcase = language_upper
+   return language_upper
 end
