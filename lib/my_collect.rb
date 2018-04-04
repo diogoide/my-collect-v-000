24d1dbcc205104af1738dce6ab10538e7490a481
Diogo Ide <diogoide@gmail.com>
2018-04-04 22:56:20 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+def  my_collect(collection)
+  i = 0
+  while i < collection.size
+    yield collection[i]
+    i += 1
+  end
+end
+    #return modified collection
 
-
+my_collect(collection) do |name|
+  name.split(" ").first
