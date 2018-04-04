2f0b1df15fabf9c8a1df9d7d9d86496756d5c63e
Diogo Ide <diogoide@gmail.com>
2018-04-04 23:02:02 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,13 @@
 def  my_collect(collection)
+  if block_given?
     i = 0
     while i < collection.length
       yield(collection[i])
         i += 1
     end
     collection
+  else
+    false 
 end
     #return modified collection
 
