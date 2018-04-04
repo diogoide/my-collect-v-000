2f69ea984b9eb5bd73932d7120cb4961389c8d1c
Diogo Ide <diogoide@gmail.com>
2018-04-04 23:24:47 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,9 @@
 def  my_collect(collection)
   if block_given?
     i = 0
+    collection = []
     while i < collection.length
-      yield(collection[i])
+      collection << yield(collection[i])
         i += 1
     end
     collection
