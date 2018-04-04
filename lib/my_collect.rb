5389409cbe9088d913d17f81dac517968b0352ec
Diogo Ide <diogoide@gmail.com>
2018-04-04 23:08:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -16,6 +16,6 @@ my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) do |name|
   name.split(" ").first
 end
 
-#my_collect(["ruby,", "javascript", "python", "objective-c"]) do |language|
-  #language.upcase
-#end
+my_collect(["ruby,", "javascript", "python", "objective-c"]) do |language|
+  language.upcase
+end
